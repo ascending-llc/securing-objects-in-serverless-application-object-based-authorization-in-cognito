35957f1d0a4273c3c799885e851f54b8c4612e39
ryo <ryo.hang@gmail.com>
2020-08-24 18:47:02 -0400

    add process to generate requirement.txt

--- build-layer.sh
+++ build-layer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eo pipefail
 rm -rf package
-cd lambda
-pip install --target ../package/python -r requirements.txt
+pipenv lock -r > requirements.txt
+pip install --target ./package/python -r requirements.txt
